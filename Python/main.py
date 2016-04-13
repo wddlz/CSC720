901437b8dacbac93e7fd14358781f35ff772d767
Ian Drosos <ian.drosos@gmail.com>
2016-04-12 21:10:58 -0400

    formatting
    format

--- Python/main.py
+++ Python/main.py
@@ -165,8 +165,8 @@ def main():
         # keywords in javascript from ECMAScript 6
         javascript_keywords = ['break', 'case', 'class', 'catch', 'const', 'continue', 'debugger', 'default', 'delete',
                                'do', 'else', 'export', 'extends', 'finally', 'for', 'function', 'if', 'import', 'in',
-                               'instanceof', 'new', 'return', 'super', 'switch', 'this', 'throw', 'try', 'typeof', 'var',
-                               'void', 'while', 'with', 'yield']
+                               'instanceof', 'new', 'return', 'super', 'switch', 'this', 'throw', 'try', 'typeof',
+                               'var', 'void', 'while', 'with', 'yield']
         with txt as t:
             for l in t:
                 increment_key_count_by_one('lineCount', seq)
